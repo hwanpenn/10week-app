7795d0dd61297a4937855c153e1d59d44c0ee0d6
hwanpenn <huangpeng@atwasoft.net>
2019-06-15 16:30:47 +0800

    cfg

--- constants/Url.js
+++ constants/Url.js
@@ -1,7 +1,8 @@
-
+import {apiurl} from '../cfg/cfg'
 // export const urlDev = 'http://172.20.10.2:7001';
 // export const urlDev = 'http://192.168.0.103:7001';
 // export const urlDev = 'http://127.0.0.1:7001';
 // export const urlDev = "http://f8fitness.com.cn"
-export const urlDev = "http://f8fitness.cn"
+// export const urlDev = "http://f8fitness.cn"
+export const urlDev = apiurl
 
